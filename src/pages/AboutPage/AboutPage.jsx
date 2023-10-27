ca7c24a1f593e18e8ff153bb4faee067c146b954
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-10-27 17:24:01 +0300

    add about page, rename pages files

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,16 @@
+import AboutInfo from '../../components/AboutInfo/AboutInfo';
+import Footer from '../../components/Footer/Footer';
+import Hero from '../../components/Hero/Hero';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <Hero />
+            <AboutInfo />
+            <Footer />
+        </main>
+    )
+    
+};
+
+export default AboutPage;
